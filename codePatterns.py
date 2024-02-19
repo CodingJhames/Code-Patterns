5746823eecc5d0ba97d28f4dc2485a3588ad3602
CodingJhames <jhames.mejia@unipamplona.edu.co>
2024-02-19 22:24:16 +0000

    update Prefix Sum

--- codePatterns.py
+++ codePatterns.py
@@ -143,17 +143,17 @@ class Solution:
 
 # Java implemetation
 
-class Solution {
-    public int pivotIndex(int[] nums) {
-        int sum = 0, leftsum = 0;
-        for (int x: nums) sum += x;
-        for (int i = 0; i < nums.length; ++i) {
-            if (leftsum == sum - leftsum - nums[i]) return i;
-            leftsum += nums[i];
-        }
-        return -1;
-    }
-}
+# class Solution {
+#     public int pivotIndex(int[] nums) {
+#         int sum = 0, leftsum = 0;
+#         for (int x: nums) sum += x;
+#         for (int i = 0; i < nums.length; ++i) {
+#             if (leftsum == sum - leftsum - nums[i]) return i;
+#             leftsum += nums[i];
+#         }
+#         return -1;
+#     }
+# }
 
 #Python implementation
 
