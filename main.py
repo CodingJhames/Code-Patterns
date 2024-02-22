6d5adc2d40eea9f02146505b2e267410e950869b
Jhames Mejía <jhames.mejia@unipamplona.edu.co>
2024-02-22 00:17:05 +0000

    Array from Permutation

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 
 # Build Array from Permutation
 
-# def buildArray(nums):
-#   n = len(nums)
-#   for i in range(0, len(nums)):
-#       nums[i]=nums[i]+(n*(nums[nums[i]]%n))
+def buildArray(nums):
+  n = len(nums)
+  for i in range(0, len(nums)):
+      nums[i]=nums[i]+(n*(nums[nums[i]]%n))
 
-#   for i in range(0, len(nums)):
-#       nums[i] = int(nums[i]/n)
+  for i in range(0, len(nums)):
+      nums[i] = int(nums[i]/n)
 
-#   return nums
+  return nums
 
-# nums = [0,2,1,5,3,4]
+nums = [0,2,1,5,3,4]
 
 # print(buildArray(nums))
 
