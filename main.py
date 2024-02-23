6ac8f25453a8e55d4e33b1af0ca2c08920a3285b
Jhames Mejía <jhames.mejia@unipamplona.edu.co>
2024-02-23 23:31:17 +0000

    string permutation

--- main.py
+++ main.py
@@ -46,16 +46,16 @@ def recur_factorial(n):
     
 # print(recur_factorial(5))
 
-# def permute(string, pocket=""):
-#   if len(string)==0:
-#     print(pocket)
-#   else:
-#     for i in range(len(string)):
-#       letter = string[i]
-#       front = string[0:i]
-#       back = string[i+1:]
-#       together = front + back
-#       permute(together, letter + pocket)
+def permute(string, pocket=""):
+  if len(string)==0:
+    print(pocket)
+  else:
+    for i in range(len(string)):
+      letter = string[i]
+      front = string[0:i]
+      back = string[i+1:]
+      together = front + back
+      permute(together, letter + pocket)
 
 # print(permute("ABC"))
 
